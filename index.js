8fd0bd4ed80d2d3c05b23883b73fdb048a66237c
Uardet99 <eduardvalls17@gmail.com>
2021-03-01 19:22:18 +0100

    Añadidos 2 nuevos endpoints

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use((request, response, next) => {
+    console.log(`Nueva petición ${new Date().toISOString()}`); 
+    next();
+});
+app.get('/date', (request, response) => {
+    const date = new Date();
+    response.send(date);
+});
+app.get('/hi', (request, response) => {
+    const message = 'Hello world';
+    response.send(message);
+});
+app.listen(2021);
